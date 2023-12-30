56b6321bc45219fe45db6b211b7d0f824b3a0eb8
dhdiagram4011 <dhdiagram@gmail.com>
2023-12-30 16:37:47 +0900

    '6-2-quiz-code-update'

--- modules/6-2.cpp
+++ modules/6-2.cpp
@@ -1,9 +1,9 @@
-/*
+
 #include <iostream>
 #include <stdlib.h>
 
 using namespace std;
-*/
+
 
 
 /*
@@ -12,7 +12,7 @@ using namespace std;
 틀리면 게임 종료
 */
 
-/*
+
 int dan[8]; //dan
 int number[9]; //number
 int problemNum[3];
@@ -23,7 +23,7 @@ int answer = {0};
 //2단부터 9단을 렌덤으로 3개 문제 출력
 int main() {
 	srand(time(NULL)); //시간을 기준으로 정수 렌덤생성
-	for (int i = 0; i <= 2; i++) {
+	for (int i = 1; i <= 3; i++) {
 		dan[i] = rand() % 10;
 		number[i] = rand() % 10;
 		std::cout << dan[i] << "*" << number[i] << endl;
@@ -31,28 +31,8 @@ int main() {
 		std::cout << "위의 곱셉의 정답을 입력하세요 :";
 		std::cin >> answer;
 			std::cout << endl;
+		//틀리면 게임 종료;
+			if (answer != dan[i] * number[i])
+				break;
 		}
 	}
-*/
-
-
-
-/*
-int main() {
-	srand(time(NULL));
-	for (int i = 0; i <= 2; i++) {
-		dan[i] = rand() % 10;
-		number[i] = rand() % 10;
-		std::cout << dan[i] << endl;
-		std::cout << endl;
-	}
-	for (int j = 0; j <= 2; j++) {
-		number[j] = rand() % 10;
-		std::cout << number[j] << ' ';
-	std:cout << endl;
-		std::cout << dan[i] * number[j] << endl;
-		std::cin >> answer;
-	}
-	std::cin >> answer;
-}
-*/
